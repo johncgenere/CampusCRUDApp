e980202a547e0729e77026be2893b63de841cda9
johncgenere <johncgenere@gmail.com>
2019-06-09 18:20:45 -0400

    File Structure & Added Forms (Non-Functional) (#1)
    * Some File Restructering
    
    * Added Forms
    
    * Added Forms to Form Pages

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import '../styles/App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className="App">
+        <Route path ="/" exact render = {
+          () => {
+            return (<Home />);
+          }
+        }/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NewStudentForm.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import '../styles/App.css';
+
+class NewStudentForm extends Component{
+  render(){
+    return(
+      <div className="App">
+        <div className="App-header">
+          <h1> Add New Student </h1>
+          <form className="ui large form" style={{width: '30%'}}>
+            <div className="ui stacked segment">
+              <div className="field">
+
+                  <input
+                    type="text"
+                    name="firstname"
+                    placeholder="First Name"
+                  />
+
+              </div>
+              <div className="field">
+                  <input
+                    type="text"
+                    name="lastname"
+                    placeholder="Last Name"
+                  />
+              </div>
+              <div className="field">
+
+                  <input
+                    type="text"
+                    name="campus"
+                    placeholder="Campus"
+                  />
+
+              </div>
+              <div className="ui fluid large black submit button"><i className="plus icon" /> Add Student </div>
+            </div>
+            <div className="ui error message"></div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewStudentForm;
