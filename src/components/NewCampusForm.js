68051f384aa4a8d0c5e0e9d176da257006800ce8
John Genere <johncgenere@gmail.com>
2019-06-09 18:31:01 -0400

    Forms Made

--- src/components/NewCampusForm.js
+++ src/components/NewCampusForm.js
@@ -3,34 +3,21 @@ import React, {Component} from 'react';
 class NewCampusForm extends Component{
   render(){
     return(
-      <form className="ui large form" onKeyPress={this.onKeyPress}>
-        <div className="ui stacked segment">
-          <div className="field">
-            <div className="ui left icon input">
-              <i className="user icon"></i>
-              <input
-                type="text"
-                name="username"
-                placeholder="Username"
-                onChange={this.handleUsernameInput}
-              />
+      <div>
+        <form className="ui large form" style={{width: '30%'}}>
+          <div className="ui stacked segment">
+          <h1> Add New Campus </h1>
+            <div className="field">
+                <input
+                  type="text"
+                  name="campus"
+                  placeholder="Campus"
+                />
             </div>
+            <div className="ui fluid large black submit button"><i className="plus icon" /> Add Campus </div>
           </div>
-          <div className="field">
-            <div className="ui left icon input">
-              <i className="lock icon"></i>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={this.handlePasswordInput}
-              />
-            </div>
-          </div>
-          <div className="ui fluid large black submit button" onClick={this.handleLogin}>Login</div>
-        </div>
-        <div className="ui error message"></div>
-      </form>
+        </form>
+      </div>
     );
   }
 }
--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -1,43 +1,36 @@
 import React, {Component} from 'react';
-import '../styles/App.css';
 
 class NewStudentForm extends Component{
   render(){
     return(
-      <div className="App">
-        <div className="App-header">
+      <div>
+        <form className="ui large form" style={{width: '30%'}}>
+          <div className="ui stacked segment">
           <h1> Add New Student </h1>
-          <form className="ui large form" style={{width: '30%'}}>
-            <div className="ui stacked segment">
-              <div className="field">
-
-                  <input
-                    type="text"
-                    name="firstname"
-                    placeholder="First Name"
-                  />
-
-              </div>
-              <div className="field">
-                  <input
-                    type="text"
-                    name="lastname"
-                    placeholder="Last Name"
-                  />
-              </div>
-              <div className="field">
-
-                  <input
-                    type="text"
-                    name="campus"
-                    placeholder="Campus"
-                  />
-
-              </div>
-              <div className="ui fluid large black submit button"><i className="plus icon" /> Add Student </div>
+            <div className="field">
+                <input
+                  type="text"
+                  name="firstname"
+                  placeholder="First Name"
+                />
             </div>
-          </form>
-        </div>
+            <div className="field">
+                <input
+                  type="text"
+                  name="lastname"
+                  placeholder="Last Name"
+                />
+            </div>
+            <div className="field">
+                <input
+                  type="text"
+                  name="campus"
+                  placeholder="Campus"
+                />
+            </div>
+            <div className="ui fluid large black submit button"><i className="plus icon" /> Add Student </div>
+          </div>
+        </form>
       </div>
     );
   }
