045a575058aca6492ba03ac878a50e666f5962e3
John Genere <johncgenere@gmail.com>
2019-06-09 18:19:07 -0400

    Added Forms to Form Pages

--- src/components/NewCampusForm.js
+++ src/components/NewCampusForm.js
@@ -3,7 +3,34 @@ import React, {Component} from 'react';
 class NewCampusForm extends Component{
   render(){
     return(
-      <h1> NewCampusForm Will be Here </h1>
+      <form className="ui large form" onKeyPress={this.onKeyPress}>
+        <div className="ui stacked segment">
+          <div className="field">
+            <div className="ui left icon input">
+              <i className="user icon"></i>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={this.handleUsernameInput}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="ui left icon input">
+              <i className="lock icon"></i>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.handlePasswordInput}
+              />
+            </div>
+          </div>
+          <div className="ui fluid large black submit button" onClick={this.handleLogin}>Login</div>
+        </div>
+        <div className="ui error message"></div>
+      </form>
     );
   }
 }
--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -1,9 +1,45 @@
 import React, {Component} from 'react';
+import '../styles/App.css';
 
 class NewStudentForm extends Component{
   render(){
     return(
-      <h1> NewStudentForm Will be Here </h1>
+      <div className="App">
+        <div className="App-header">
+          <h1> Add New Student </h1>
+          <form className="ui large form" style={{width: '30%'}}>
+            <div className="ui stacked segment">
+              <div className="field">
+
+                  <input
+                    type="text"
+                    name="firstname"
+                    placeholder="First Name"
+                  />
+
+              </div>
+              <div className="field">
+                  <input
+                    type="text"
+                    name="lastname"
+                    placeholder="Last Name"
+                  />
+              </div>
+              <div className="field">
+
+                  <input
+                    type="text"
+                    name="campus"
+                    placeholder="Campus"
+                  />
+
+              </div>
+              <div className="ui fluid large black submit button"><i className="plus icon" /> Add Student </div>
+            </div>
+            <div className="ui error message"></div>
+          </form>
+        </div>
+      </div>
     );
   }
 }
