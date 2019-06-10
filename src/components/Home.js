2d0e36a835cc6fd6d2cd812ac9d436fb08fc9682
John Genere <johncgenere@gmail.com>
2019-06-10 01:13:50 -0400

    Made Forms and added Campus Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,12 @@
 import React, {Component} from 'react';
-import NewCampusForm from './NewCampusForm';
-import NewStudentForm from './NewStudentForm';
-import EditCampusForm from './EditCampusForm';
-import EditStudentForm from './EditStudentForm';
 
 class Home extends Component {
   render(){
     return(
-      <div>
-        <NewStudentForm />
-        <NewCampusForm />
-        <EditStudentForm />
-        <EditCampusForm />
+      <div className="App">
+        <div className="App-header">
+          <h1> Home </h1>
+        </div>
       </div>
     );
   }
