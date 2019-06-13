83447ccd6d43dcb117cdc776a7ef5b5b3af134e4
John Genere <johncgenere@gmail.com>
2019-06-13 04:25:52 -0400

    fixed root index file to import store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import App from './components/App';
-import reducers from './reducers';
+import store from './store';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
